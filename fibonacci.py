41283d1daa890adf4465a4c63fae8fcad50fa9c4
David Slater <davidslater@users.noreply.github.com>
2024-06-20 15:32:22 -0700

    Fix base cases for the Fibonacci function

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,10 @@
 # Returns the nth Fibonacci number
 def fib(n):
-    if n == 1:
+    if n < 0:
+        raise ValueError("n must be a non-negative integer")
+    elif n == 0:
+        return 0
+    elif n == 1:
         return 1
     else:
         return fib(n-1) + fib(n-2)
